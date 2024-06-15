bf3c8a9fa4172a1f822df2a7dd6e1468ebaa8cdf
d.onglassyn <d.onglassyn@alemagro.com>
2024-06-15 13:00:20 +0500

    Write Repositories for Task Manager

---
+++ backend/app/Repositories/ExternalRepositories/Contract/Category/CategoryRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories\ExternalRepositories\Contract\Category;
+
+class CategoryRepository
+{
+
+}
---
+++ backend/app/Repositories/ExternalRepositories/Contract/Category/CategoryRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories\ExternalRepositories\Contract\Category;
+
+interface CategoryRepositoryInterface
+{
+
+}
